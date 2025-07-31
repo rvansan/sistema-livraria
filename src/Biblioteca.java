8e6e64c5393322bfc71f9cf1821c978eeeef7b70
rvansan <vansan.rafael@gmail.com>
2025-07-31 19:11:17 -0300

    classes da livraria

---
+++ src/Biblioteca.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Biblioteca {
+    List<Livro> livros;
+    List<Autor> autores;
+    List<Livro> emprestimos;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,51 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        boolean continuarExibicao = false;
+
+        do{
+
+            System.out.println("Você Gostaria de ver os livros disponíveis?");
+            System.out.println("Se SIM, digite 's'");
+            System.out.println("Se NÃO, digite 'n'");
+
+            Scanner scanner = new Scanner(System.in);
+            String opcao = scanner.next();
+
+            if(opcao.equals("s")) {
+                exibirLivros();
+                continuarExibicao = true;
+            } else if(opcao.equals("n")) {
+                System.out.println("***************************");
+                System.out.println("Saindo...");
+                System.out.println("***************************\n");
+                continuarExibicao = false;
+            } else {
+                System.out.println("***************************");
+                System.out.println("Opção Inválida!");
+                System.out.println("***************************\n");
+                continuarExibicao = true;
+            }
+
+        }while(continuarExibicao);
+    }
+
+    private static void exibirLivros(){
+        System.out.println("***************************");
+        System.out.println("Exibindo livros.");
+
+        Livro livro = new Livro();
+        livro.id = 1;
+        livro.titulo = "titulo";
+        livro.estaDisponivel = true;
+        livro.dataCadastro = LocalDate.now();
+        livro.dataAtualizacao = LocalDate.now();
+
+        System.out.println("***************************\n");
     }
 }
